2d4c413c35a39eb34fbc719e19ec04f77e68e0bc
Grant Cummings <grantcummings4429@gmail.com>
2024-02-28 17:50:52 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1>Grant is the champion</h1>";
